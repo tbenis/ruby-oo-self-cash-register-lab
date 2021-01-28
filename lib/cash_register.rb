23135cec76c4cf64e40347f63cf0466b80f34eb2
Benis Tambe <will.jenifer@yahoo.com>
2021-01-28 18:58:51 -0500

    commit tree 1d93e429acd4be6f32cec9bfdf42de720df152ac

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
 
+class CashRegister
+    attr_accessor :total, :discount, :apply_discount, :items, :previously_added
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+    def add_item(title, price, quantity=1)
+        @total += price * quantity
+        counting = quantity
+        while counting > 0
+            @items.push(title)
+            counting -= 1
+        end
+        @previously_added = price * quantity
+        puts @previously_added
+    end
+
+
+    def apply_discount()
+        if discount > 0
+            @total = (@total * ((100.0 - @discount.to_f)/100)).to_i
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        puts @previously_added
+        @total -= self.previously_added
+    end
+end
